b566d2feb669b6c9f1ad95d54826b7d2ae2002b4
bmonagan <bernardmonagan@gmail.com>
2026-04-20 18:56:28 -0400

    Initialize FastAPI application and include authentication and user routers

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from auth-microservice!")
+# main.py
+from fastapi import FastAPI
+from routes import auth, users
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+app.include_router(auth.router, prefix="/auth")
+app.include_router(users.router)  # already has /users prefix

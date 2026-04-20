c5d9367e791fb566e9a0852c0ff3232f67b85269
bmonagan <bernardmonagan@gmail.com>
2026-04-20 19:04:18 -0400

    Fix import path for settings in database.py for consistency

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 # database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
-from config import settings
+from src.config import settings
 
 engine = create_engine(
     settings.DATABASE_URL,

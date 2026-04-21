cd2253c8e0d61c17fd3938ce4283d18919eb67b9
bmonagan <bernardmonagan@gmail.com>
2026-04-21 16:22:49 -0400

    config: Initialize rate limiter in FastAPI app

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 # main.py
 from fastapi import FastAPI
+from slowapi import Limiter
+from slowapi.util import get_remote_address
 from src.routes import auth, users
 
+limiter = Limiter(key_func=get_remote_address)
+
 app = FastAPI()
+app.state.limiter = limiter
 
 app.include_router(auth.router, prefix="/auth")
 app.include_router(users.router)  # already has /users prefix
